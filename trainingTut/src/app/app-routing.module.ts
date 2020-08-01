b706ce0d865e07c12203adde13da58f18c3aebe1
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-01 01:34:24 -0500

    update routing

--- trainingTut/src/app/app-routing.module.ts
+++ trainingTut/src/app/app-routing.module.ts
@@ -2,15 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'signup', pathMatch: 'full' },
-    {
-        path: 'signup',
-        loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
-    },
-    {
-        path: 'training',
-        loadChildren: () => import('./modules/_training/training.module').then((m) => m.TrainingModule),
-    },
+    { path: '', redirectTo: 'welcome/login', pathMatch: 'full' },
     {
         path: 'welcome',
         loadChildren: () => import('./modules/_welcome/welcome.module').then((m) => m.WelcomeModule),
