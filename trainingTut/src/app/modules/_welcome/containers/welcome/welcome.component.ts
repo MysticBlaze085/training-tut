9867feb1f42dfa740281a7e5a427eb93bc8f300c
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-03 16:59:02 -0500

    added sidenav

--- trainingTut/src/app/modules/_welcome/containers/welcome/welcome.component.ts
+++ trainingTut/src/app/modules/_welcome/containers/welcome/welcome.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
-  selector: 'app-welcome',
-  templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-welcome',
+    templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WelcomeComponent implements OnInit {
+    constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {}
 }
--- trainingTut/src/app/modules/_welcome/welcome.module.ts
+++ trainingTut/src/app/modules/_welcome/welcome.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { WELCOME_CONTAINERS } from './containers';
 import { WELCOME_COMPONENTS } from './components';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [...WELCOME_CONTAINERS, ...WELCOME_COMPONENTS],
-    imports: [CommonModule, WelcomeRoutingModule],
+    imports: [CommonModule, WelcomeRoutingModule, SharedModule],
     exports: [...WELCOME_CONTAINERS, ...WELCOME_COMPONENTS],
 })
 export class WelcomeModule {}
