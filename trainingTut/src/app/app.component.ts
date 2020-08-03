f013225da96f8937aedf90afd05e5c7afde9c346
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-03 17:26:52 -0500

    sidenav changes

--- trainingTut/src/app/app.component.ts
+++ trainingTut/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+    @ViewChild('sidenav', { static: true }) sidenav;
+}
