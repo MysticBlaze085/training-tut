ef076e2953a7d89becd58bfa9747a6030fca096c
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-18 19:54:16 -0500

    added dropdown form field

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,9 @@ import { SignupComponent } from './signup/signup.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from '../modules/shared/shared.module';
 import { LoginComponent } from './login/login.component';
-import { DynamicFormInputComponent } from '../modules/shared';
 
 @NgModule({
-    declarations: [SignupComponent, LoginComponent, DynamicFormInputComponent],
+    declarations: [SignupComponent, LoginComponent],
     imports: [CommonModule, AuthRoutingModule, SharedModule],
     exports: [SignupComponent, LoginComponent],
 })
--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MaterialsModule } from '.';
+import { DynamicFormInputComponent, MaterialsModule } from '.';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    declarations: [],
+    declarations: [DynamicFormInputComponent],
     imports: [CommonModule, MaterialsModule, ReactiveFormsModule, FormsModule, FlexLayoutModule],
-    exports: [MaterialsModule, ReactiveFormsModule, FormsModule, FlexLayoutModule],
+    exports: [MaterialsModule, ReactiveFormsModule, FormsModule, FlexLayoutModule, DynamicFormInputComponent],
 })
 export class SharedModule {}
