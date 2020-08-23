3dc069f0deb3116896f222e77c12f4e441c0487f
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-23 17:10:09 -0500

    auth state added

--- src/app/modules/shared/containers/navigation/header/header.component.ts
+++ src/app/modules/shared/containers/navigation/header/header.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { AuthService } from '../../../../../_services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
     @Output() sidenavToggle: EventEmitter<void> = new EventEmitter<void>();
+    isAuth: boolean;
+    authSubscription: Subscription;
 
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.authSubscription = this.authService.authChange.subscribe((authStatus) => (this.isAuth = authStatus));
+    }
+
+    ngOnDestroy() {
+        this.authSubscription.unsubscribe();
+    }
 
     onToggleSidenav() {
         this.sidenavToggle.emit();
--- src/app/modules/shared/containers/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/modules/shared/containers/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { AuthService } from '../../../../../_services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-sidenav-list',
     templateUrl: './sidenav-list.component.html',
     styleUrls: ['./sidenav-list.component.scss'],
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
     @Output() closeSidenav: EventEmitter<void> = new EventEmitter<void>();
+    isAuth: boolean;
+    authSubscription: Subscription;
 
-    constructor() {}
+    constructor(private authService: AuthService) {}
 
-    ngOnInit(): void {}
+    ngOnInit(): void {
+        this.authSubscription = this.authService.authChange.subscribe((authState) => (this.isAuth = authState));
+    }
+
+    ngOnDestroy() {
+        this.authSubscription.unsubscribe();
+    }
 
     onCloseSidenav() {
         this.closeSidenav.emit();
