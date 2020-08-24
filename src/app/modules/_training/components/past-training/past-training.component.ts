a89958e03f1b8bd926447f435a2ac98d2858ad02
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-23 19:34:30 -0500

    pagination added

--- src/app/modules/_training/components/past-training/past-training.component.ts
+++ src/app/modules/_training/components/past-training/past-training.component.ts
@@ -1,4 +1,5 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Exercise } from 'src/app/_interfaces';
@@ -14,6 +15,7 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
     dataSource = new MatTableDataSource<Exercise>();
 
     @ViewChild(MatSort, { static: true }) sort: MatSort;
+    @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
     constructor(private trainingService: TrainingService) {}
 
@@ -23,5 +25,10 @@ export class PastTrainingComponent implements OnInit, AfterViewInit {
 
     ngAfterViewInit() {
         this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+    }
+
+    doFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
     }
 }
