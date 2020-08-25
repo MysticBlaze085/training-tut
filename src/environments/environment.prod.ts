5b9aec5ac70201ac849fd79864ae86fca8754c53
Melissa Langhausen <melissaslanghausen@gmail.com>
2020-08-24 22:28:42 -0500

    udpated prodcution env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+    production: true,
+    firebase: {
+        apiKey: 'AIzaSyCfAC6KnKtwXhqP8RWsAEclAi4bwtK6ygI',
+        authDomain: 'ng-fitness-tracker-d23a6.firebaseapp.com',
+        databaseURL: 'https://ng-fitness-tracker-d23a6.firebaseio.com',
+        projectId: 'ng-fitness-tracker-d23a6',
+        storageBucket: 'ng-fitness-tracker-d23a6.appspot.com',
+        messagingSenderId: '671123654181',
+        appId: '1:671123654181:web:23e8a439be933f130ae1cc',
+        measurementId: 'G-RFQR9CG3GB',
+    },
 };
